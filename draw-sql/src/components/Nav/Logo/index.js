591529a498bbb94a738c3e53311c410eb71b3aa3
pipe <luis.angulo@koombea.com>
2021-12-20 14:35:17 -0500

    Fix responsiveness and logo background

--- draw-sql/src/components/Nav/Logo/index.js
+++ draw-sql/src/components/Nav/Logo/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./styles.scss";
 import logo from "../../../assets/images/logo.png";
-import logotext from "../../../assets/images/logotext.png";
 import share from "../../../assets/images/share.png";
 
 function Logo() {
   return (
     <div className="logo">
-      <div className="logo__elements">
-        <img className="logo__image" src={logo} alt="sql" />
-        <img className="logo__text" src={logotext} alt="sqltext" />
-      </div>
+      <img className="logo__image" src={logo} alt="sql" />
+      <h4 className="logo__text">draw<span className="sql__text">SQL</span></h4>
       <p className="logo__file">File</p>
       <div className="logo__share">
         <p>Share</p>
